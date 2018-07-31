f1ef7760cc20ca1102594b7eb141093f7fce499e
Ally Donaldson <afd@imperial.ac.uk>
2018-07-31 12:43:08 +0100

    Script to find wrong code examples.

---
+++ find_wrong_code_examples.py
@@ -0,0 +1,67 @@
+import os
+from random import Random
+from hypothesis.internal.conjecture.data import ConjectureData
+import sys
+from gen_prog import gen
+import hashlib
+import subprocess
+
+EXAMPLES = os.path.join(os.path.dirname(__file__), "examples")
+
+LIMIT = 5
+
+
+def results_for_o1_and_o2_differ(sourcename):
+    try:
+        proc = subprocess.Popen(["gcc", "-O1", "-o", "opt1", "-I", "runtime", sourcename], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc.communicate()
+        if proc.returncode != 0:
+            return False
+        proc = subprocess.Popen(["gcc", "-O2", "-o", "opt2", "-I", "runtime", sourcename], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if proc.returncode != 0:
+            return False
+        proc = subprocess.Popen(["./opt1"], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout1, stderr1 = proc.communicate(timeout=3)
+        if proc.returncode != 0:
+            return False
+        proc = subprocess.Popen(["./opt2"], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout2, stderr2 = proc.communicate(timeout=3)
+        if proc.returncode != 0:
+            return False
+        return stdout1 != stdout2
+    except subprocess.SubprocessError:
+        return False
+
+
+if __name__ == '__main__':
+
+    try:
+        os.makedirs(EXAMPLES)
+    except FileExistsError:
+        pass
+
+    rnd = Random()
+
+    found = 0
+    i = 0
+    while found < LIMIT:
+        i += 1
+        print("Iter", i)
+        data = ConjectureData(
+            max_length=2**29,
+            draw_bytes=lambda self, n: rnd.getrandbits(8 * n).to_bytes(
+                n, byteorder='big'
+            )
+        )
+
+        sourcename = "example.c"
+
+        gen(data, sourcename)
+
+        if results_for_o1_and_o2_differ(sourcename):
+            found += 1
+            print("Found", found)
+            result = bytes(data.buffer)
+            name = hashlib.sha1(result).hexdigest()[:16]
+            with open(os.path.join(EXAMPLES, name), 'wb') as outfile:
+                outfile.write(result)
