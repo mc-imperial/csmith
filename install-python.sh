b68e25d4cbe9eca04e18e2871d889dc54399d01a
David R. MacIver <david@drmaciver.com>
2018-07-31 14:26:00 +0100

    Docker environment for running wrong code experiments

---
+++ install-python.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'export PYENV_VERSION=3.6.1' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
+source ~/.bashrc
+
+pyenv install 3.6.1
+pyenv global 3.6.1
+
+pip install --upgrade pip
+pip install hypothesis
