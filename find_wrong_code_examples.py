4ce925c3adcdcf34e901f81a5419569f53f9e8c9
David R. MacIver <david@drmaciver.com>
2018-08-03 10:29:10 +0100

    Add more logging to better debug behaviour

--- find_wrong_code_examples.py
+++ find_wrong_code_examples.py
@@ -8,26 +8,33 @@ import subprocess
 
 EXAMPLES = os.path.join(os.path.dirname(__file__), "examples")
 
-LIMIT = 5
+LIMIT = 100
 
 
 def results_for_o1_and_o2_differ(sourcename):
     try:
+        print("Compiling")
         proc = subprocess.Popen(["gcc", "-O1", "-o", "opt1", "-I", "runtime", sourcename], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        proc.communicate()
+        _, err = proc.communicate()
         if proc.returncode != 0:
+            print(err)
             return False
         proc = subprocess.Popen(["gcc", "-O2", "-o", "opt2", "-I", "runtime", sourcename], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        _, err = proc.communicate()
         if proc.returncode != 0:
+            print(err)
             return False
+        print("Running 1")
         proc = subprocess.Popen(["./opt1"], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout1, stderr1 = proc.communicate(timeout=3)
+        stdout1, stderr1 = proc.communicate(timeout=0.5)
         if proc.returncode != 0:
             return False
+        print("Running 2")
         proc = subprocess.Popen(["./opt2"], encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout2, stderr2 = proc.communicate(timeout=3)
+        stdout2, stderr2 = proc.communicate(timeout=0.5)
         if proc.returncode != 0:
             return False
+        print("Ran")
         return stdout1 != stdout2
     except subprocess.SubprocessError:
         return False
@@ -57,6 +64,8 @@ if __name__ == '__main__':
         sourcename = "example.c"
 
         gen(data, sourcename)
+
+        print("Generated")
 
         if results_for_o1_and_o2_differ(sourcename):
             found += 1
